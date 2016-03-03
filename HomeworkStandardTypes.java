a52b56228bd46b2b9e7c728356aae1dfb989cc16
Cristina <c.titu@yahoo.com>
2016-03-03 19:15:23 +0200

    Tema Tipuri primitive de date

---
+++ HomeworkStandardTypes.java
@@ -0,0 +1,41 @@
+package test_package;
+
+public class HomeworkStandardTypes {
+	
+	public static void main(String[] args){
+		
+		byte b = 50;
+		b = 127;
+		
+		short s = 502;
+		s = -1245; 
+		
+		int a = 55589;
+		a = 2648;
+		
+		long l = 512L;
+		l = 2315L;
+		
+		double d = 15.26;
+		d= 123.6587456 ;
+		
+		float f = 15.26f; 
+		f = 12f ;
+		
+		char c = '&';
+		c = 'r';
+		
+		boolean boo = true;
+		boo = false ;
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(a);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(boo);
+	}
+
+}
