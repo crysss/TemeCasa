a5ed40a8a891db45b9a93505c56e290085ff153a
Cristina <c.titu@yahoo.com>
2016-03-09 14:38:54 +0200

    Object creation from a defined class using new operator

---
+++ SomethingIsWrong.java
@@ -0,0 +1,22 @@
+package test_package;
+
+public class SomethingIsWrong {
+    public static void main(String[] args) {
+        Rectangle myRect = new Rectangle ();
+        myRect.width = 40;
+        myRect.height = 50;
+        System.out.println("myRect's area is " + myRect.area());
+    }
+}
+
+class Rectangle {
+int width;
+int height;
+
+int area (){
+	int result;	
+	result = width * height;
+	return result;
+}
+	
+}
